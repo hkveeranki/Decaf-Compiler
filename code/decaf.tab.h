89ea300d1f0c01043b72038b48f618eccbc38f24
Hemanth Kumar Veeranki <hems.india1997@gmail.com>
2016-11-11 20:59:44 +0530

    Added Additional Tests
    Signed-off-by: Hemanth Kumar Veeranki <hemanthveeranki@gmail.com>

---
+++ code/decaf.tab.h
@@ -0,0 +1,98 @@
+/* A Bison parser, made by GNU Bison 3.0.4.  */
+
+/* Bison interface for Yacc-like parsers in C
+
+   Copyright (C) 1984, 1989-1990, 2000-2015 Free Software Foundation, Inc.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+/* As a special exception, you may create a larger work that contains
+   part or all of the Bison parser skeleton and distribute that work
+   under terms of your choice, so long as that work isn't itself a
+   parser generator using the skeleton or a modified version thereof
+   as a parser skeleton.  Alternatively, if you modify or redistribute
+   the parser skeleton itself, you may (at your option) remove this
+   special exception, which will cause the skeleton and the resulting
+   Bison output files to be licensed under the GNU General Public
+   License without this special exception.
+
+   This special exception was added by the Free Software Foundation in
+   version 2.2 of Bison.  */
+
+#ifndef YY_YY_DECAF_TAB_H_INCLUDED
+# define YY_YY_DECAF_TAB_H_INCLUDED
+/* Debug traces.  */
+#ifndef YYDEBUG
+# define YYDEBUG 0
+#endif
+#if YYDEBUG
+extern int yydebug;
+#endif
+
+/* Token type.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+  enum yytokentype
+  {
+    CLASS = 258,
+    CALLOUT = 259,
+    RETURN = 260,
+    BREAK = 261,
+    CONTINUE = 262,
+    IF = 263,
+    ELSE = 264,
+    FOR = 265,
+    COMMA = 266,
+    SC = 267,
+    BOOLEAN = 268,
+    CHAR = 269,
+    STRING = 270,
+    TYPE = 271,
+    ID = 272,
+    VOID = 273,
+    INTEGER = 274,
+    OB = 275,
+    CB = 276,
+    OSB = 277,
+    CSB = 278,
+    OP = 279,
+    CP = 280,
+    COND_AND = 281,
+    COND_OR = 282,
+    NOT = 283,
+    ADD = 284,
+    SUB = 285,
+    MUL = 286,
+    DIV = 287,
+    MOD = 288,
+    LT = 289,
+    GT = 290,
+    LE = 291,
+    GE = 292,
+    EQUAL = 293,
+    NOT_EQUAL = 294,
+    EQ = 295,
+    ADDEQ = 296,
+    SUBEQ = 297
+  };
+#endif
+
+/* Value type.  */
+
+
+extern YYSTYPE yylval;
+
+int yyparse (void);
+
+#endif /* !YY_YY_DECAF_TAB_H_INCLUDED  */
